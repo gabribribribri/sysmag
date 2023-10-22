b2d474003d4627e648055b2a9610e3ba9a477eff
gabribribribri <fuelanemo149@gmail.com>
2023-10-22 17:19:29 +0200

    First full stable version

--- disks.nix
+++ disks.nix
@@ -2,14 +2,14 @@
 
 {
     fileSystems."/mnt/Mars" = {
-        device = "/dev/disk/by-uuid/8d06421a-8954-40db-b53e-2d082cfe460b";
+        device = "/dev/disk/by-uuid/f94127c3-dfd8-4bb6-ad3a-3c18c7b89345";
         fsType = "ext4";
-        options = [ "nofail" ];
+        # options = [ "nofail" ];
     };
 
-    fileSystems."/mnt/Jupiter" = {
-        device = "/dev/disk/by-uuid/3241de8b-34ee-487f-968f-439e3f6e3b0e";
-        fsType = "ext4";
-        options = [ "nofail" ];
-    };
+    # fileSystems."/mnt/Jupiter" = {
+    #     device = "/dev/disk/by-uuid/3241de8b-34ee-487f-968f-439e3f6e3b0e";
+    #     fsType = "ext4";
+    #     # options = [ "nofail" ];
+    # };
 }
--- rebuild-test.sh
+++ rebuild-test.sh
@@ -3,4 +3,4 @@
 # Github token
 #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
 
-sudo nixos-rebuild test -I nixos-config=./all.nix
+sudo nixos-rebuild test -I nixos-config=./all.nix --upgrade --verbose
