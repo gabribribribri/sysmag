478151aec566aa9aa58da2e38f4a1afe57b4d1bb
gabribribribri <fuelanemo149@gmail.com>
2023-11-23 11:51:28 +0100

    started Awesome business

--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ chmod +x deck/install_nix.sh deck/install_hm.sh deck/hm-switch.sh
 chmod +x hades/init_altgr-weur.sh hades/rebuild-switch.sh hades/rebuild-test.sh
 
 # Create config files symlinks
-ln -s $HOME/sysmag/config/helix/ $HOME/.config/helix # Helix config
+ln -s $HOME/sysmag/config/helix/ $HOME/.config/ # Helix config
+ln -s $HOME/sysmag/config/awesome $HOME/.config/ # Awesome config
