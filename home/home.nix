8431d1be60a0ea6ecd89a6d71aac60a83b8d79da
gabribribribri <fuelanemo149@gmail.com>
2024-08-18 18:09:58 +0200

    QU'EST-CE TU RACONTES

--- home/home.nix
+++ home/home.nix
@@ -30,6 +30,15 @@
     home.sessionVariables = {
     EDITOR = "hx";
   };
+  xdg.configFile = {
+    "awesome".source = ./app/awesome;
+    "btop".source = ./app/btop;
+    # "clangd.source = ./app/clangd;
+    "helix".source = ./app/helix;
+    "kitty".source = ./app/kitty;
+    "picom".source = ./app/picom;
+    "rofi".source = ./app/rofi;
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
