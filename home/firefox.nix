c01a6850134b2f3613ae9ff23239e5f5ff46c314
gabribribribri <fuelanemo149@gmail.com>
2024-08-19 17:12:40 +0200

    hide single tab false

--- home/firefox.nix
+++ home/firefox.nix
@@ -9,6 +9,7 @@
       settings = {
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "browser.chrome.toolbar_tips" = false;
+        "animatedFox.hideSingleTab" = false;
         "browser.toolbars.bookmarks.visibility" = "never";
         "extensions.pocket.enabled" = false;
       };
