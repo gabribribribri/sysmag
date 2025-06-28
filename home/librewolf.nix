bdb96cb22ca0cf5102ef2193bb1596a45cfde3a6
gabribribribri <fuelanemo149@gmail.com>
2025-06-28 15:00:26 +0200

    added librewolf goodies

--- home/librewolf.nix
+++ home/librewolf.nix
@@ -6,11 +6,26 @@
       name = "Gaybe's Config";
       isDefault = true;
       settings = {
+        # Old Firefox stuff, don't know if necessary with librewolf anymore
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "browser.chrome.toolbar_tips" = false;
-        "animatedFox.hideSingleTab" = false;
         "browser.toolbars.bookmarks.visibility" = "never";
         "extensions.pocket.enabled" = false;
+
+        "identity.fxaccounts.enabled" = true;
+        "webgl.disabled" = false;
+        "browser.newtabpage.enabled" = false;
+        "browser.search.suggest.enabled" = true;
+        "browser.search.suggest.enabled.private" = true;
+        "browser.urlbar.showSearchSuggestionsFirst" = true;
+        "places.history.enabled" = true;
+
+        # !!! DID NOT FIND YET
+        # Remember browsing and download history
+        # Remember search and form history
+        # Save and fill addresses
+        # Save and fill payment methods
+        # Ask to save password and sub categories
       };
     };
   };
