577c21c4788d0808a06922f1f41f4af9a1acb1d7
gabribribribri <fuelanemo149@gmail.com>
2025-06-28 14:23:03 +0200

    added zsh goodies

--- home/zsh.nix
+++ home/zsh.nix
@@ -5,12 +5,26 @@
       enable = true;
       enableCompletion = true;
       autosuggestion.enable = true;
+      autosuggestion.strategy = [
+        "completion"
+        "match_prev_cmd"
+        "history"
+      ];
+      enableBashCompletion = true;
       syntaxHighlighting.enable = true;
+      syntaxHighlighting.highlighters = [
+        "main"
+        "brackets"
+        "pattern"
+        "regexp"
+        "root"
+        "line"
+      ];
       shellAliases = {
         lg = "lazygit";
         qalc = "qalc -c";
         lsd = "lsd -lAg";
-        nrs-hades = "sudo nixos-rebuild switch --flake ~/sysmag/#hades $1";
+        # nrs-hades = "sudo nixos-rebuild switch --flake ~/sysmag/#hades $1";
       };
       oh-my-zsh = {
         enable = true;
